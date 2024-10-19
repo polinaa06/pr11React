ab1e539218ec936330f523be91122be8fde24cc4
polinaa06 <bezvinnaya00@bk.ru>
2024-10-19 14:26:18 +0300

    pr11react

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import { Banner } from '../../components/Banner/Banner'
+import { SliderElem } from '../../components/SliderElem/SliderElem'
+
+export function HomePage(){
+    return(
+        <section>
+            <Banner/>
+            <SliderElem/>
+        </section>
+    )
+}
